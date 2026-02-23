789559c84bd3be5c641664b9d14d73e7f3d9a734
gustavbagger <gustavbagger@me.com>
2026-02-23 12:51:13 +1100

    seperated into helpers

--- main.go
+++ main.go
@@ -2,53 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"os"
-	"strconv"
 
 	pr "github.com/fxtlabs/primes"
 )
 
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Println("wrong arguments")
-		return
-	}
-	var omegaMax int
-	var err error
-	omegaMax, err = strconv.Atoi(os.Args[1])
-	if err != nil {
-		return
-	}
-	omegaMin := omegaMax
-	if len(os.Args) == 3 {
-		omegaMin, err = strconv.Atoi(os.Args[2])
-		if err != nil {
-			return
-		}
-	}
-	//careful here, we need len(primeList)>= omega
-	primeList := pr.Sieve(173)
-	for omega := omegaMax; omega >= omegaMin; omega-- {
-		sBest := 0
-		currentBest := float64(int(1) << (omega + 1))
-		for s := 1; s <= omega; s++ {
-			delta := deltaSum(primeList[omega-s : omega])
-			if delta <= 0.0 {
-				break
-			}
-			currentTry := (2.0 + float64(s-1)/delta) * float64(int(1)<<(omega+1-s))
-			if currentTry < currentBest {
-				currentBest = currentTry
-				sBest = s
-			}
-		}
-		fmt.Printf(
-			"%5.1e>p>%5.1e ---- o,s = %v,%v\n",
-			math.Pow(currentBest, 16),
-			Prod(primeList[:omega]),
-			omega,
-			sBest,
-		)
-	}
+
+	primeList := pr.Sieve(2000)
+
+	fmt.Printf("%5.2e\n", Prod(append(primeList[:33], primeList[218])))
+	pritnIntervals()
 }
