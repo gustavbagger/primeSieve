7bc82a51a49e6c594560e80bd847b2343a2904a3
gustavbagger <gustavbagger@me.com>
2026-02-24 11:33:28 +1100

    old overflowing version

--- helpers.go
+++ helpers.go
@@ -17,15 +17,15 @@ func deltaSum(list []int) float64 {
 	return sum + 1
 }
 
-func Prod(list []int) float64 {
-	prod := 1.0
+func Prod(list []int) int {
+	prod := 1
 	for _, p := range list {
-		prod *= float64(p)
+		prod *= p
 	}
 	return prod
 }
 
-func pritnIntervals() {
+func printIntervals() {
 	if len(os.Args) < 2 || len(os.Args) > 3 {
 		fmt.Println("wrong arguments")
 		return
@@ -67,4 +67,94 @@ func pritnIntervals() {
 			sBest,
 		)
 	}
+}
+
+func indexesToValues(indexes, allValues []int) []int {
+	output := make([]int, len(indexes))
+	for i, index := range indexes {
+		output[i] = allValues[index]
+	}
+	return output
+}
+
+func validIndexSet(indexes, allValues []int) bool {
+	return pr.IsPrime(Prod(indexesToValues(indexes, allValues)) + 1)
+}
+
+func treeSearch(position, currentProduct, bound int, indexes, currentGuess, allValues []int) {
+	if position == len(indexes) {
+		if validIndexSet(currentGuess, allValues) {
+			fmt.Println("--------------------------------------------")
+			fmt.Printf("%v\n", indexesToValues(currentGuess, allValues))
+		}
+		return
+	}
+
+	index := indexes[position]
+	i := allValues[index]
+
+	nextProduct := currentProduct * i
+	if nextProduct > bound {
+		return
+	}
+
+	currentGuess = append(currentGuess, index)
+	currentProduct = nextProduct
+
+	treeSearch(position+1, currentProduct, bound, indexes, currentGuess, allValues)
+
+	for {
+		nextProduct = currentProduct * i
+		if nextProduct > bound {
+			return
+		}
+		currentGuess = append(currentGuess, index)
+		currentProduct = nextProduct
+
+		treeSearch(position+1, currentProduct, bound, indexes, currentGuess, allValues)
+	}
+}
+
+func suffSmall(bound int, indexes, allValues []int) bool {
+	return Prod(indexesToValues(indexes, allValues)) <= bound
+}
+
+type Status int
+
+const (
+	Continue Status = iota
+	Backtrack
+	Stop
+)
+
+func recursiveLoop(currentDepth, maxDepth, maxIndex, upperBound int, indexes, primeList []int) Status {
+	if currentDepth == maxDepth {
+		fmt.Println(suffSmall(upperBound, indexes, primeList))
+		treeSearch(0, 1, upperBound, indexes, []int{}, primeList)
+		return Continue
+	}
+	startIndex := 0
+	if currentDepth > 0 {
+		startIndex = indexes[currentDepth-1] + 1
+	}
+
+	for i := startIndex; i <= maxIndex-(maxDepth-currentDepth); i++ {
+		indexes[currentDepth] = i
+
+		if !suffSmall(upperBound, indexes, primeList) {
+			return Backtrack
+		}
+
+		status := recursiveLoop(currentDepth+1, maxDepth, maxIndex, upperBound, indexes, primeList)
+
+		switch status {
+		case Continue:
+			continue
+		case Backtrack:
+			continue
+		case Stop:
+			return Stop
+		}
+	}
+	return Backtrack
 }
--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
-
+	omega := 34
+	upperBound := 1 << 62
 	primeList := pr.Sieve(2000)
 
-	fmt.Printf("%5.2e\n", Prod(append(primeList[:33], primeList[218])))
-	pritnIntervals()
-
+	maxIndex := len(primeList) - 1
+	indexes := make([]int, omega)
+	prod := Prod([]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 53, 53, 67, 71, 73, 83, 89, 89, 97, 101, 103, 107, 107, 107, 109, 109, 137, 137, 139, 139, 149, 149, 149, 149, 149, 149, 149, 149, 149, 149, 149, 151, 151, 151, 157, 157, 157, 163, 163, 173, 179})
+	fmt.Println(prod + 1)
+	fmt.Println(pr.IsPrime(prod + 1))
+	recursiveLoop(0, omega, maxIndex, upperBound, indexes, primeList)
+	fmt.Println("--------------------------------------------")
 }
